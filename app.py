d9b98a78289e16f67587a67dbbeeccb8be9b7141
Tejas Jain <1tejasjain@gmail.com>
2023-07-16 21:27:06 +0530

    Code Refactored and Added Template st.dataframe for Data

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 import streamlit as st
-
-def main():
-    st.set_page_config(page_title="Courtroom Testimonies Verifier",
-                       page_icon=":scales:")
-    st.title("Courtroom Testimonies Verifier :scales:")
-    st.subheader("Give Courtroom Testimonies and Find AI based truthfulness score with inconsistencies")
-
+import random
+import pandas as pd
+def sidebar():
     with st.sidebar:
         "# Testimonies"
         def TestimoniesInput(party, placeholderTestimony):
@@ -17,17 +13,49 @@ def main():
         accusedTestimony = TestimoniesInput("Accused", "Enter the testimony of the case from Accused's side")
         policeTestimony = TestimoniesInput("Police", "Enter the testimony of the case from Police's side")
         
-        # #Witness Button [ Hiding to simplify the UI and Primary Logic]
-        # if "button" not in st.session_state:
-        #         st.session_state.button = False
-        # witnessTestimonies = ""          
-        # if st.button("Add Witness", key="witnessBtn", disabled=st.session_state.button): 
-        #     st.session_state.button = True
-        #     witnessTestimonies = TestimoniesInput("Witness", "Enter the testimony of the case from Witness 1's side")
-        
-        # if st.button("Process", type="primary"):
-        #     with st.spinner("Processing"):
-        #         print("Processing")
+        if st.button("Process", type="primary"):
+            with st.spinner("Processing"):
+                print("Processing")
+
+
+def mainpage():
+    st.title("Courtroom Testimonies Verifier :scales:")
+    st.subheader("Give Courtroom Testimonies and Find AI based truthfulness score with inconsistencies")
+    
+    # col1, col2 = st.columns([4, 1])
+
+    # def inconsistencies():
+
+    df = pd.DataFrame(
+        {
+            "name": ["Roadmap", "Extras", "Issues"],
+            "url": ["https://roadmap.streamlit.app", "https://extras.streamlit.app", "https://issues.streamlit.app"],
+            "stars": [random.randint(0, 1000) for _ in range(3)],
+            "views_history": [[random.randint(0, 5000) for _ in range(30)] for _ in range(3)],
+        }
+    )
+
+    st.dataframe(
+        df,
+        column_config={
+            "name": "App name",
+            "stars": st.column_config.NumberColumn(
+                "Github Stars",
+                help="Number of stars on GitHub",
+                format="%d ⭐",
+            ),
+            "url": st.column_config.LinkColumn("App URL"),
+            "views_history": st.column_config.LineChartColumn(
+                "Views (past 30 days)", y_min=0, y_max=5000
+            ),
+        },
+        hide_index=True,
+    )
+
+def main():
+    st.set_page_config(page_title="Courtroom Testimonies Verifier",page_icon=":scales:")
+    sidebar()
+    mainpage()
 
 
 
