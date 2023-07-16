d3ab59e56c5cefd87185a230fbd2a289c0c2df9e
Tejas Jain <1tejasjain@gmail.com>
2023-07-17 01:12:55 +0530

    Split the text using . character

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 import streamlit as st
 import random
 import pandas as pd
+from langchain.text_splitter import CharacterTextSplitter
+
+
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator='.',
+        chunk_size=40,
+        chunk_overlap=10,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
 def sidebar():
     with st.sidebar:
         "# Testimonies"
@@ -12,112 +26,61 @@ def sidebar():
         victimTestimony = TestimoniesInput("Victim", "Enter the testimony of the case from Victim's side")
         accusedTestimony = TestimoniesInput("Accused", "Enter the testimony of the case from Accused's side")
         policeTestimony = TestimoniesInput("Police", "Enter the testimony of the case from Police's side")
-        
+
+
         if st.button("Process", type="primary"):
             with st.spinner("Processing"):
                 print("Processing")
+                st.session_state.victimChunks = victimTestimony.split(".")
+                st.session_state.accusedChunks = accusedTestimony.split(".")
+                st.session_state.policeChunks = policeTestimony.split(".")
+                
 
 
 def mainpage():
     st.title("Courtroom Testimonies Verifier :scales:")
     st.subheader("Give Courtroom Testimonies and Find AI based truthfulness score with inconsistencies")
+    st.write(st.session_state.accusedChunks)
+#     df = pd.DataFrame(
+#         {
+#             "Party": ["Accused", "Victim", "Evidence"],
+#             "DoubtFulStatements": [
+# f"""
+# Original: He said something\n
+# Mis-Match: "They said something else
 
-    df = pd.DataFrame(
-        {
-            "Party": ["Accused", "Victim", "Evidence"],
-            "DoubtFulStatements": [
-f"""
-Original: He said something\n
-Mis-Match: "They said something else
+# Original: He said something\n
+# Mis-Match: "They said something else
+# \n\n
+# Original: He said something
+# Mis-Match: "They said something else
+# """, 
+# f"""
+# Original: He said something 
+# Mis-Match: "They said something else
 
-Original: He said something\n
-Mis-Match: "They said something else
-\n\n
-Original: He said something
-Mis-Match: "They said something else
-""", 
-f"""
-Original: He said something 
-Mis-Match: "They said something else
+# Original: He said something 
+# Mis-Match: "They said something else
 
-Original: He said something 
-Mis-Match: "They said something else
+# Original: He said something 
+# Mis-Match: "They said something else
+# """,
+# f"""
+# Original: He said something 
+# Mis-Match: "They said something else
 
-Original: He said something 
-Mis-Match: "They said something else
-""",
-f"""
-Original: He said something 
-Mis-Match: "They said something else
+# Original: He said something 
+# Mis-Match: "They said something else
 
-Original: He said something 
-Mis-Match: "They said something else
+# Original: He said something 
+# Mis-Match: "They said something else
+# """
 
-Original: He said something 
-Mis-Match: "They said something else
-"""
-
-            ],
-            "stars": [random.randint(0, 1000) for _ in range(3)],
-            # "views_history": [[random.randint(0, 5000) for _ in range(30)] for _ in range(3)],
-        }
-    )
-    # pd.set_option('display.max_colwidth', None)
-# [
-#                 {
-#                     "Original Statement1: ": "He said something",
-#                     "Mis-Match1": "They said something else"
-#                 }, 
-#                 {
-#                     "Original Statement2: ": "He said something",
-#                     "Mis-Match2": "They said something else"
-#                 }, 
-#                 {
-#                     "Original Statement3: ": "He said something",
-#                     "Mis-Match3": "They said something else"
-#                 }, 
-#             ]
-
-    # # Create a DataFrame with a multiline string
-    # data = {
-    #     'Name': ['Alice', 'Bob'],
-    #     'Description': ['This is a<br>multiline\nstring.', 'Another\nmultiline\nstring.']
-    # }
-    
-    # df = pd.DataFrame(data)
-    # pd.set_option('display.max_colwidth', None)
-    # print(df)
-    # # # Display the DataFrame with multiline strings
-    # st.write(df.to_html(escape=False, index=False), unsafe_allow_html=True)
-    # data = {
-    #     'Name': ['Alice', 'Bob'],
-    #     'Description': ['This is a\nmultiline\nstring.', 'Another\nmultiline\nstring.']
-    # }
-
-    # df = pd.DataFrame(data)
-
-    # # Adjust display options
-    # pd.set_option('display.max_colwidth', None)  # Display full contents of each cell
-
-    # # Print the DataFrame
-    # print(df)
-    st.write(df.to_html(escape=False, index=False), unsafe_allow_html=True)
-    st.dataframe(
-        df,
-        column_config={
-            "name": "App name",
-            "stars": st.column_config.NumberColumn(
-                "Github Stars",
-                help="Number of stars on GitHub",
-                format="%d ⭐"
-            ),
-            # "url": st.column_config.LinkColumn("App URL"),
-            "views_history": st.column_config.LineChartColumn(
-                "Views (past 30 days)", y_min=0, y_max=5000
-            ),
-        },
-        hide_index=True,
-    )
+#             ],
+#             "stars": [random.randint(0, 1000) for _ in range(3)]
+#         }
+#     )
+#     st.write(df.to_html(escape=False, index=False), unsafe_allow_html=True)
 
 def main():
     st.set_page_config(page_title="Courtroom Testimonies Verifier",page_icon=":scales:")
