a563a6db87c9bff5fa7072bc4630820514a1d98f
Tejas Jain <1tejasjain@gmail.com>
2023-07-17 00:39:32 +0530

    Tried Various Ways to display mismatched pair of statements

--- app.py
+++ app.py
@@ -21,20 +21,87 @@ def sidebar():
 def mainpage():
     st.title("Courtroom Testimonies Verifier :scales:")
     st.subheader("Give Courtroom Testimonies and Find AI based truthfulness score with inconsistencies")
-    
-    # col1, col2 = st.columns([4, 1])
-
-    # def inconsistencies():
 
     df = pd.DataFrame(
         {
-            "name": ["Roadmap", "Extras", "Issues"],
-            "url": ["https://roadmap.streamlit.app", "https://extras.streamlit.app", "https://issues.streamlit.app"],
+            "Party": ["Accused", "Victim", "Evidence"],
+            "DoubtFulStatements": [
+f"""
+Original: He said something\n
+Mis-Match: "They said something else
+
+Original: He said something\n
+Mis-Match: "They said something else
+\n\n
+Original: He said something
+Mis-Match: "They said something else
+""", 
+f"""
+Original: He said something 
+Mis-Match: "They said something else
+
+Original: He said something 
+Mis-Match: "They said something else
+
+Original: He said something 
+Mis-Match: "They said something else
+""",
+f"""
+Original: He said something 
+Mis-Match: "They said something else
+
+Original: He said something 
+Mis-Match: "They said something else
+
+Original: He said something 
+Mis-Match: "They said something else
+"""
+
+            ],
             "stars": [random.randint(0, 1000) for _ in range(3)],
-            "views_history": [[random.randint(0, 5000) for _ in range(30)] for _ in range(3)],
+            # "views_history": [[random.randint(0, 5000) for _ in range(30)] for _ in range(3)],
         }
     )
+    # pd.set_option('display.max_colwidth', None)
+# [
+#                 {
+#                     "Original Statement1: ": "He said something",
+#                     "Mis-Match1": "They said something else"
+#                 }, 
+#                 {
+#                     "Original Statement2: ": "He said something",
+#                     "Mis-Match2": "They said something else"
+#                 }, 
+#                 {
+#                     "Original Statement3: ": "He said something",
+#                     "Mis-Match3": "They said something else"
+#                 }, 
+#             ]
 
+    # # Create a DataFrame with a multiline string
+    # data = {
+    #     'Name': ['Alice', 'Bob'],
+    #     'Description': ['This is a<br>multiline\nstring.', 'Another\nmultiline\nstring.']
+    # }
+    
+    # df = pd.DataFrame(data)
+    # pd.set_option('display.max_colwidth', None)
+    # print(df)
+    # # # Display the DataFrame with multiline strings
+    # st.write(df.to_html(escape=False, index=False), unsafe_allow_html=True)
+    # data = {
+    #     'Name': ['Alice', 'Bob'],
+    #     'Description': ['This is a\nmultiline\nstring.', 'Another\nmultiline\nstring.']
+    # }
+
+    # df = pd.DataFrame(data)
+
+    # # Adjust display options
+    # pd.set_option('display.max_colwidth', None)  # Display full contents of each cell
+
+    # # Print the DataFrame
+    # print(df)
+    st.write(df.to_html(escape=False, index=False), unsafe_allow_html=True)
     st.dataframe(
         df,
         column_config={
@@ -42,9 +109,9 @@ def mainpage():
             "stars": st.column_config.NumberColumn(
                 "Github Stars",
                 help="Number of stars on GitHub",
-                format="%d ⭐",
+                format="%d ⭐"
             ),
-            "url": st.column_config.LinkColumn("App URL"),
+            # "url": st.column_config.LinkColumn("App URL"),
             "views_history": st.column_config.LineChartColumn(
                 "Views (past 30 days)", y_min=0, y_max=5000
             ),
